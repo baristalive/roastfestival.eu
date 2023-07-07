41029c415764def23e009472666d34817112782c
Tomas Coufal <coufal.tom@gmail.com>
2023-07-07 21:26:44 +0200

    feat: initial prototype
    Signed-off-by: Tomas Coufal <tcoufal@redhat.com>

---
+++ app/data.ts
@@ -0,0 +1,24 @@
+export const data = {
+  date: "16-17/9/2023",
+  place: "Káznice, Brno",
+  title: "The first coffee roasters festival in Brno",
+  tickets: "Tickets",
+  contact: "Contact us",
+  programLoading: true,
+  programLoadingText: "Wa are still roasting the festival program for you",
+  aboutTitle: "About",
+  about: "..."
+}
+
+export const program = [
+  { time: "9.00 - 9.30", title: "Festival Opening Ceremony"},
+  { time: "9.00 - 9.30", title: "Festival Opening Ceremony"},
+  { time: "9.00 - 9.30", title: "Festival Opening Ceremony"},
+  { time: "9.00 - 9.30", title: "Festival Opening Ceremony"},
+  { time: "9.00 - 9.30", title: "Festival Opening Ceremony"},
+  { time: "9.00 - 9.30", title: "Festival Opening Ceremony"},
+  { time: "9.00 - 9.30", title: "Festival Opening Ceremony"},
+  { time: "9.00 - 9.30", title: "Festival Opening Ceremony"},
+  { time: "9.00 - 9.30", title: "Festival Opening Ceremony"}
+];
+
