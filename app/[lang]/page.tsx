c3321e2e095ef434bf321a27097c653b3de195b6
Tomas Coufal <coufal.tom@gmail.com>
2023-07-08 17:43:09 +0200

    feat: add graphics
    Signed-off-by: Tomas Coufal <coufal.tom@gmail.com>

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,22 +1,18 @@
-'use client';
+"use client";
 
-import Image from 'next/image'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faArrowAltCircleDown,
-} from "@fortawesome/free-regular-svg-icons";
-import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { useParams } from "next/navigation";
-import { dictionaries, SupportedLanguages} from '../dictionaries/all'
-import { faAt, faEarthAmerica, faEarthEurope } from '@fortawesome/free-solid-svg-icons';
-import Link from 'next/link';
-
+import { dictionaries, SupportedLanguages } from "../dictionaries/all";
+import Link from "next/link";
+import FacebookIcon from "../icons/facebook";
+import InstagramIcon from "../icons/instagram";
+import AtIcon from "../icons/at";
+import ArrowIcon from "../icons/arrow";
 
 const Page1 = () => {
   const params = useParams();
-  const lang = dictionaries[params.lang as SupportedLanguages]
+  const lang = dictionaries[params.lang as SupportedLanguages];
   return (
-    <section className="h-screen flex flex-col p-8 content-stretch">
+    <section className="h-screen flex flex-col p-8 content-stretch page1">
       <div className="grow flex md:flex-row flex-col md:justify-center gap-20">
         <header className="flex flex-row gap-8 w-full max-w-[1900px]">
           <div className="hidden md:block basis-1/6 font-medium text-2xl">
@@ -35,11 +31,11 @@ const Page1 = () => {
           <nav className="hidden md:block text-right font-medium text-2xl">
             <div>{lang.contact}</div>
             <div className="flex justify-end gap-4 mt-4">
-              <a className="h-10 w-10 text-[2.5rem] text-center" href="#">
-                <FontAwesomeIcon icon={faFacebook} />
+              <a href="#">
+                <FacebookIcon />
               </a>
-              <a className="h-10 w-10 instagram inverted" href="#">
-                <FontAwesomeIcon icon={faInstagram} />
+              <a href="#">
+                <InstagramIcon />
               </a>
             </div>
           </nav>
@@ -63,7 +59,7 @@ const Page1 = () => {
         <div className="max-w-md mx-auto">{lang.programLoadingText}</div>
       </div>
       <div className="text-6xl text-center">
-        <FontAwesomeIcon icon={faArrowAltCircleDown} />
+        <ArrowIcon />
       </div>
     </section>
   );
@@ -74,17 +70,19 @@ const About = () => {
   const lang = dictionaries[params.lang as SupportedLanguages];
   return (
     <section className="inverted py-20 text-center">
-      <h2 className="uppercase font-medium text-3xl py-20 ">{lang.about.title}</h2>
+      <h2 className="uppercase font-medium text-3xl py-20 ">
+        {lang.about.title}
+      </h2>
       <div className="space-y-10 mx-auto px-6 md:px-0 text-2xl max-w-3xl">
-        {lang.about.text.map(p => <p key={p}>{p}</p>)}
+        {lang.about.text.map((p) => (
+          <p key={p}>{p}</p>
+        ))}
       </div>
     </section>
   );
 };
 
-const Filler = () => (
-  <section className="inverted h-screen"/>
-)
+const Filler = () => <section className="inverted filler" />;
 
 const Program = () => {
   const params = useParams();
@@ -99,7 +97,7 @@ const Program = () => {
       {lang.program.map((d, idx) => (
         <div
           key={d.title}
-          className={`${idx % 2 ? "" : "inverted"} p-8 md:p-20`}
+          className={`${idx % 2 ? "" : "inverted"} p-2 md:p-20`}
         >
           <div className="md:hidden py-20 px-6 flex">
             <h2 className="grow uppercase font-medium pt-2 text-left text-3xl">
@@ -119,7 +117,7 @@ const Program = () => {
               {d.schedule.map((i) => (
                 <div
                   key={i.time}
-                  className="m-6 md:m-0 md:grid md:grid-cols-3 md:gap-4 md:py-2 md:px-0"
+                  className="m-2 md:m-0 md:grid md:grid-cols-3 md:gap-4 py-3 md:px-0"
                 >
                   <dt className="text-xl font-bold">{i.time}</dt>
                   <dd className="text-xl md:col-span-2 md:mt-0">{i.title}</dd>
@@ -137,7 +135,7 @@ const BuyTickets = () => {
   const params = useParams();
   const lang = dictionaries[params.lang as SupportedLanguages];
   return (
-    <section className="text-center mx-auto p-20">
+    <section className="text-center mx-auto py-20">
       <a
         className="rounded-full py-6 px-12 border  border-current text-2xl font-bold"
         href="#"
@@ -156,14 +154,14 @@ const Footer = () => {
       <nav className="text-center mx-auto font-medium text-2xl">
         <div>{lang.contact}</div>
         <div className="flex justify-center gap-4 mt-2 pb-40">
-          <a className="h-10 w-10 text-[2.5rem] text-center" href="#">
-            <FontAwesomeIcon icon={faFacebook} />
+          <a className="inverted" href="#">
+            <FacebookIcon />
           </a>
-          <a className="h-10 w-10 instagram" href="#">
-            <FontAwesomeIcon icon={faInstagram} />
+          <a className="inverted" href="#">
+            <InstagramIcon />
           </a>
-          <a className="h-10 w-10 instagram" href="mailto:info@baristalive.cz">
-            <FontAwesomeIcon icon={faAt} />
+          <a className="h-[34px] w-[34px]" href="mailto:info@baristalive.cz">
+            <AtIcon />
           </a>
         </div>
       </nav>
@@ -176,17 +174,13 @@ const Footer = () => {
   );
 };
 
+const Map = () => <section className="inverted h-screen" />;
 
-const Map = () => (
-  <section className="inverted h-screen">
-  </section>
-);
-
-export async function generateStaticParams() {
-  return Object(dictionaries).keys().map((lang : string) => ({ lang }));
-}
-
-export default function Home({params} : { params: {lang: SupportedLanguages }}) {
+export default function Home({
+  params,
+}: {
+  params: { lang: SupportedLanguages };
+}) {
   return (
     <div className="overlay">
       <div className="lang inverted uppercase">
---
+++ app/icons/arrow.tsx
@@ -0,0 +1,41 @@
+const ArrowIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="50"
+    height="50"
+    viewBox="0 0 50 50"
+    style={{ display: "inline-block"}}
+  >
+    <g transform="translate(-935 -973)">
+      <g
+        transform="translate(13.17 -25.551)"
+      >
+        <line
+          y2="23.809"
+          transform="translate(947.257 1011.5)"
+          fill="none"
+          stroke="#000"
+          stroke-width="4"
+        />
+        <path
+          d="M0,0,11.184,11.184,0,22.368"
+          transform="translate(958.441 1024.418) rotate(90)"
+          fill="none"
+          stroke="#000"
+          stroke-width="4"
+        />
+      </g>
+      <g
+        transform="translate(935 973)"
+        fill="none"
+        stroke="#000"
+        stroke-width="1"
+      >
+        <circle cx="25" cy="25" r="25" stroke="none" />
+        <circle cx="25" cy="25" r="24.5" fill="none" />
+      </g>
+    </g>
+  </svg>
+);
+
+export default ArrowIcon;
