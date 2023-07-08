9dd057c95d1bcce98871f55b739488adfbf5c862
Tomas Coufal <coufal.tom@gmail.com>
2023-07-08 17:52:46 +0200

    fix: dir for static assets in gh-pages
    Signed-off-by: Tomas Coufal <coufal.tom@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+// trim off `<owner>/`
+const repo = (process.env.GITHUB_REPOSITORY || "").replace(/.*?\//, "");
+
 const nextConfig = {
   output: "export",
+  ...(process.env.GITHUB_ACTIONS && {
+    basePath: `/${repo}`,
+  })
 };
 
 module.exports = nextConfig
--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
+// trim off `<owner>/`
+const repo = (process.env.GITHUB_REPOSITORY || "").replace(/.*?\//, "");
+
 module.exports = {
   plugins: {
+    "postcss-url": {
+      url: ({ url }) =>
+        url[0] === "/" && process.env.GITHUB_ACTIONS ? `/${repo}/${url}` : url,
+    },
     tailwindcss: {},
     autoprefixer: {},
   },
-}
+};
