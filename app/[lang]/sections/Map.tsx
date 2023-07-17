edd6590b83fd589257e836cab773a6218222efd9
Tomas Coufal <coufal.tom@gmail.com>
2023-07-17 18:28:07 +0200

    feat: add location section
    Signed-off-by: Tomas Coufal <coufal.tom@gmail.com>

--- app/[lang]/sections/Map.tsx
+++ app/[lang]/sections/Map.tsx
@@ -7,9 +7,29 @@ export const Map = () => {
   const lang = dictionaries[params.lang as SupportedLanguages];
   return (
     <section className="inverted h-screen py-20 text-center">
-      <h2 className="uppercase font-medium text-4xl py-20 ">
+      <h2 className="uppercase font-medium text-4xl pt-20 pb-36">
         {lang.location.title}
       </h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 place-content-center">
+        <div></div>
+        <div className="space-y-20 lg:text-2xl font-medium text-xl">
+          {
+            // eslint-disable-next-line @next/next/no-img-element
+          }<img
+            className="mx-auto block"
+            src="/kaznice.png"
+            alt="Logo Káznice"
+          />
+          <p>
+            Bratislavská 249/68,
+            <br />
+            602 00 Brno-Střed Zábrdovice
+          </p>
+          <div className="bg-[#fff800] rounded-full w-10 h-10 inline-block m-2" />
+          <div className="bg-[#fff800] rounded-full w-10 h-10 inline-block m-2" />
+          <div className="bg-[#fff800] rounded-full w-10 h-10 inline-block m-2" />
+        </div>
+      </div>
     </section>
   );
 };
