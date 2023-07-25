c3e8d45bb24d0d3f5c103fa9932131a10b7f1175
Tomas Coufal <coufal.tom@gmail.com>
2023-07-26 00:28:43 +0200

    feat: add og/twitter images base
    Signed-off-by: Tomas Coufal <coufal.tom@gmail.com>

---
+++ app/[lang]/twitter-image.tsx
@@ -0,0 +1,5 @@
+import oGImage, { generateImageMetadata as ogGenerateImageMetadata } from "./opengraph-image";
+
+export const generateImageMetadata = ogGenerateImageMetadata
+
+export default oGImage
