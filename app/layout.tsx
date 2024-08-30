396c2c59124ba44e3a77456023c9610cc63fc0ca
Tomas Coufal <coufal.tom@gmail.com>
2024-08-30 22:50:02 +0200

    feat: add dynamic favicon
    Signed-off-by: Tomas Coufal <coufal.tom@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,37 @@ import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
 import dictionaries from "./dictionaries/all";
 
+
 const figtree = Figtree({ subsets: ["latin", "latin-ext"] });
 export const metadata: Metadata = {
   title: "ROAST!",
   description: "Coffee roasters festival in Brno",
+  icons: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '48x48',
+      url: '/icon-light-48.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '192x192',
+      url: '/icon-light-192.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/svg+xml',
+      url: '/icon-light.svg',
+      media: '(prefers-color-scheme: light)',
+    },
+    {
+      rel: 'icon',
+      type: 'image/svg+xml',
+      url: '/icon-dark.svg',
+      media: '(prefers-color-scheme: dark)',
+    },
+  ],
 };
 
 export async function generateStaticParams() {
