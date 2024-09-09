38f05aab1765aade049f67a3a1647fe33f083ff0
Tomas Coufal <coufal.tom@gmail.com>
2024-09-09 10:27:27 +0200

    fix: schedule responsiveness update for low res wide screens
    Signed-off-by: Tomas Coufal <coufal.tom@gmail.com>

--- app/[lang]/sections/Program.tsx
+++ app/[lang]/sections/Program.tsx
@@ -34,11 +34,42 @@ export const Program = () => {
       const activeNav = selector<HTMLAnchorElement>("a.active");
       const state = Flip.getState(".pills");
       activeNav[0].appendChild(activeBubble[0]);
+      activeNav[1].appendChild(activeBubble[1]);
 
       Flip.from(state, { duration: 2, ease: "power1.inOut", scale: true });
     },
     { scope: ref, dependencies: [tab] },
   );
+
+  const dayToggle = (
+    <ul className="elevate card pills flex rounded-e-full rounded-s-full text-center font-medium">
+      {lang.program.map((day, idx) => (
+        <li className="relative z-0 w-full p-2" key={day.$ref}>
+          {idx === 0 && <div className="active absolute inset-2 z-0" />}
+          <a
+            href={`#program_day_${idx + 1}`}
+            className={`z-10 flex w-full cursor-pointer flex-col whitespace-nowrap px-8 py-2 2xl:px-16 2xl:py-4 ${idx === tab ? "active" : ""} ${idx === 0 ? "rounded-s-full" : ""} ${idx === lang.program.length - 1 ? "rounded-e-full" : ""}`}
+            onClick={changeTabTo(idx)}
+          >
+            <h3 className="z-10 text-xl font-bold 2xl:text-3xl">
+              {
+                lang.programDays[
+                  day.$ref as keyof typeof lang.programDays
+                ].name
+              }
+            </h3>
+            <span className="z-10 text-base 2xl:text-xl">
+              {
+                lang.programDays[
+                  day.$ref as keyof typeof lang.programDays
+                ].date
+              }
+            </span>
+          </a>
+        </li>
+      ))}
+    </ul>
+    )
 
   return (
     <section
@@ -54,37 +85,11 @@ export const Program = () => {
           <h2 className="p-4 pl-8 text-3xl font-bold md:pl-20 2xl:text-6xl">
             Program
           </h2>
-          <div className="p-4">
-            <ul className="elevate card pills flex rounded-e-full rounded-s-full text-center font-medium">
-              {lang.program.map((day, idx) => (
-                <li className="relative z-0 w-full p-2" key={day.$ref}>
-                  {idx === 0 && <div className="active absolute inset-2 z-0" />}
-                  <a
-                    href={`#program_day_${idx + 1}`}
-                    className={`z-10 flex w-full cursor-pointer flex-col whitespace-nowrap px-8 py-2 2xl:px-16 2xl:py-4 ${idx === tab ? "active" : ""} ${idx === 0 ? "rounded-s-full" : ""} ${idx === lang.program.length - 1 ? "rounded-e-full" : ""}`}
-                    onClick={changeTabTo(idx)}
-                  >
-                    <h3 className="z-10 text-xl font-bold 2xl:text-3xl">
-                      {
-                        lang.programDays[
-                          day.$ref as keyof typeof lang.programDays
-                        ].name
-                      }
-                    </h3>
-                    <span className="z-10 text-base 2xl:text-xl">
-                      {
-                        lang.programDays[
-                          day.$ref as keyof typeof lang.programDays
-                        ].date
-                      }
-                    </span>
-                  </a>
-                </li>
-              ))}
-            </ul>
+          <div className="p-4 lg:hidden xl:block">
+            {dayToggle}
           </div>
         </div>
-        <div className="grow">
+        <div className="grow lg:grid grid-cols-2 xl:block">
           <>
             {lang.program.every((day) => day.schedule.length === 0) && (
               <div className="mx-auto flex  h-full w-full items-center justify-center gap-4 p-12 py-48">
@@ -107,10 +112,25 @@ export const Program = () => {
               <React.Fragment key={day.$ref}>
                 {day.schedule.length > 0 && (
                   <div
-                    className={`schedule inset-0 top-4 flex flex-col will-change-auto ${idx !== tab ? "hidden" : ""}`}
+                    className={`schedule flex justify-start items-center flex-col will-change-auto ${idx !== tab ? "hidden lg:block xl:hidden" : ""}`}
                   >
-                    <div className="card elevate relative m-3 flex flex-col justify-between rounded-2xl pb-4">
-                      <div className="hidden lg:grid grid-cols-[repeat(24,_minmax(0,_1fr))] p-4 pt-10 text-center">
+                    <div className="hidden lg:block xl:hidden text-center">
+                    <h3 className="z-10 text-xl font-bold 2xl:text-3xl">
+                      {
+                        lang.programDays[
+                          day.$ref as keyof typeof lang.programDays
+                        ].name
+                      }
+                    </h3>
+                    <span className="z-10 text-base 2xl:text-xl">
+                      {
+                        lang.programDays[
+                          day.$ref as keyof typeof lang.programDays
+                        ].date
+                      }
+                    </span></div>
+                    <div className="card elevate relative m-3 flex flex-col justify-between rounded-2xl py-4">
+                      <div className="hidden xl:grid grid-cols-[repeat(24,_minmax(0,_1fr))] p-4 pt-10 text-center">
                         <div className="col-span-2"></div>
                         {hours.map((h) => (
                           <div className="col-span-2" key={h}>
@@ -118,7 +138,7 @@ export const Program = () => {
                           </div>
                         ))}
                       </div>
-                      <div className="absolute inset-0 z-0 hidden lg:grid grid-cols-[repeat(24,_minmax(0,_1fr))] divide-x-2 divide-dotted divide-gray-200 p-4 pt-20">
+                      <div className="absolute inset-0 z-0 hidden xl:grid grid-cols-[repeat(24,_minmax(0,_1fr))] divide-x-2 divide-dotted divide-gray-200 p-4 pt-20">
                         <div className="col-span-3"></div>
                         {hours.map((h, idx) => (
                           <div
@@ -135,7 +155,7 @@ export const Program = () => {
                           className="program-track relative mx-4 grid grid-cols-[repeat(24,_minmax(0,_1fr))] rounded-2xl py-4 text-xl"
                         >
                           <div
-                            className="col-span-full lg:col-span-3 row-start-1 flex flex-col items-center justify-center p-2 text-center"
+                            className="col-span-full xl:col-span-3 row-start-1 flex flex-col items-center justify-center p-2 text-center"
                             style={{ gridRowEnd: t.rows + 1 }}
                           >
                             <StationIcon station={t.track} />
@@ -153,7 +173,7 @@ export const Program = () => {
                             ] as Presenter;
                             return (
                               <div
-                                className="mx-0.5 program-slot-wrapper"
+                                className="mx-2 xl:mx-0.5 program-slot-wrapper"
                                 key={`${lang.program[tab].$ref}_${presenter?.name}_${idx}`}
                                 style={{
                                   "--gridColumnStart": `${(s.start - 10) * 2 + 4}`,
@@ -165,13 +185,13 @@ export const Program = () => {
                                 ) : (
                                   <>
                                     <Modal {...presenter}>
-                                      <div className="program-slot elevate my-1 overflow-hidden rounded-lg px-3 py-1  md:py-2 lg:text-left text-center">
-                                        <div className="col-span-full lg:hidden text-base">
+                                      <div className="program-slot elevate my-1 overflow-hidden rounded-lg px-3 py-1  md:py-2 xl:text-left text-center">
+                                        <div className="col-span-full xl:hidden text-base">
                                           {getTimeString(s.start)} - {getTimeString(s.end)}
                                         </div>
                                         <h4 className="font-bold text-lg">{presenter.name}</h4>
                                         {presenter.subheading && (
-                                          <i className="text-base text-nowrap text-ellipsis">
+                                          <i className="text-base">
                                             {presenter.subheading}
                                           </i>
                                         )}
@@ -184,6 +204,7 @@ export const Program = () => {
                           })}
                         </div>
                       ))}
+                      <div className="mx-auto">{lang.programDisclaimer}</div>
                     </div>
                   </div>
                 )}
@@ -191,6 +212,7 @@ export const Program = () => {
             ))}
           </>
         </div>
+        <div className="lg:hidden mx-auto mb-4">{dayToggle}</div>
       </div>
     </section>
   );
