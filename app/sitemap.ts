7d000834b6675bc3dffb4208e4df8429e2d42be4
Tomas Coufal <coufal.tom@gmail.com>
2025-09-17 12:45:30 +0200

    fix: push stoptime by an hour and add to schedule sitemap
    Signed-off-by: Tomas Coufal <coufal.tom@gmail.com>

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,16 +4,22 @@ import { dictionaries } from "./dictionaries/all";
 export const dynamic = "force-static";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  return Object.keys(dictionaries).map((l) => ({
-    alternates: {
-      languages: Object.fromEntries(
-        Object.keys(dictionaries).map((alternateLang) => [
-          alternateLang === "cz" ? "cs" : alternateLang,
-          `https://roastfestival.eu/${alternateLang}`,
-        ]),
-      ),
-    },
-    lastModified: new Date(),
-    url: `https://roastfestival.eu/${l}`,
-  }));
+  const sites = ["", "/program"];
+
+  return sites
+    .map((s) =>
+      Object.keys(dictionaries).map((l) => ({
+        alternates: {
+          languages: Object.fromEntries(
+            Object.keys(dictionaries).map((alternateLang) => [
+              alternateLang === "cz" ? "cs" : alternateLang,
+              `https://roastfestival.eu/${alternateLang}${s}`,
+            ]),
+          ),
+        },
+        lastModified: new Date(),
+        url: `https://roastfestival.eu/${l}${s}`,
+      })),
+    )
+    .flat();
 }
