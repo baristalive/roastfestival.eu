33a3d64e421efe7e2e81471bae84faaf2dc3c8d1
Tomas Coufal <coufal.tom@gmail.com>
2025-09-18 17:04:53 +0200

    chore: cleanup print pages
    Signed-off-by: Tomas Coufal <coufal.tom@gmail.com>

--- app/[lang]/print/[day]/image/[room]/page.tsx
+++ app/[lang]/print/[day]/image/[room]/page.tsx
@@ -5,8 +5,8 @@ import dictionaries, {
   Day,
   Presenter,
   SupportedLanguages,
+  Track,
 } from "@/app/dictionaries/all";
-import "./print.css";
 import "../../../../../globals.css";
 import { toPng } from "html-to-image";
 import { getRoomCategory, RoomCategory } from "./utils";
@@ -45,7 +45,7 @@ const Schedule = (props: SchedulePropsType) => {
   }
 
   const schedule = day.schedule.filter((s) =>
-    [room].includes(s.track as keyof typeof dictionaries.en.programCategory),
+    [room].includes(s.track as Track),
   );
 
   if (
@@ -73,59 +73,56 @@ const Schedule = (props: SchedulePropsType) => {
           {lang.programDays[params.day].name}{" "}
           {lang.programDays[params.day].date}
         </h2>
-        {schedule
-          .filter((t) => [room].includes(t.track))
-          .map((t, idx) => {
-            const count = t.schedule.length;
-            const style =
-              count === 1 || room === "espresso_milk"
-                ? { display: "flex", flexDirection: "column" }
-                : count > 3
-                  ? { display: "flex" }
-                  : { gridTemplateColumns: `repeat(${count}, minmax(0, 1fr))` };
-            const lineStyle = count > 3 ? { flexBasis: "25%" } : {};
-            return (
-              <div
-                className="grid flex-wrap items-stretch justify-center gap-10 px-10"
-                key={`${t.track}_${idx}`}
-                style={style as CSSProperties}
-              >
-                {t.schedule.map((i, idx) => (
-                  <div
-                    className="flex flex-col gap-6"
-                    style={lineStyle}
-                    key={idx}
-                  >
-                    {i.map((s) => {
-                      const presenter = lang.presenters[
-                        s.$ref as keyof typeof lang.presenters
-                      ] as Presenter;
-                      return (
-                        <div
-                          className="schedule-item elevate relative my-1 flex flex-col items-center rounded-lg p-2"
-                          key={`${t.track}_${idx}_${s.$ref}`}
-                        >
-                          <h4 className="relative mb-1 text-2xl font-bold">
-                            {presenter.lang && (
-                              <span className="elevate absolute left-full ml-2 inline-block aspect-square h-8 w-8 rounded-full bg-[var(--white)] p-1 text-base font-bold uppercase text-[var(--black)]">
-                                {presenter.lang}
-                              </span>
-                            )}
-                            {presenter.name}
-                          </h4>
-                          <i className="text-lg">
-                            {s.start} - {s.end}{" "}
-                            {presenter.subheading &&
-                              `| ${presenter.subheading}`}
-                          </i>
-                        </div>
-                      );
-                    })}
-                  </div>
-                ))}
-              </div>
-            );
-          })}
+        {schedule.map((t, idx) => {
+          const count = t.schedule.length;
+          const style =
+            count === 1 || room === "espresso_milk"
+              ? { display: "flex", flexDirection: "column" }
+              : count > 3
+                ? { display: "flex" }
+                : { gridTemplateColumns: `repeat(${count}, minmax(0, 1fr))` };
+          const lineStyle = count > 3 ? { flexBasis: "25%" } : {};
+          return (
+            <div
+              className="grid flex-wrap items-stretch justify-center gap-10 px-10"
+              key={`${t.track}_${idx}`}
+              style={style as CSSProperties}
+            >
+              {t.schedule.map((i, idx) => (
+                <div
+                  className="flex flex-col gap-6"
+                  style={lineStyle}
+                  key={idx}
+                >
+                  {i.map((s) => {
+                    const presenter = lang.presenters[
+                      s.$ref as keyof typeof lang.presenters
+                    ] as Presenter;
+                    return (
+                      <div
+                        className="elevate relative my-1 flex flex-col items-center rounded-lg bg-[var(--accent)] p-2 text-center text-[var(--white)]"
+                        key={`${t.track}_${idx}_${s.$ref}`}
+                      >
+                        <h4 className="relative mb-1 text-2xl font-bold">
+                          {presenter.lang && (
+                            <span className="elevate absolute left-full ml-2 inline-block aspect-square h-8 w-8 rounded-full bg-[var(--white)] p-1 text-base font-bold uppercase text-[var(--black)]">
+                              {presenter.lang}
+                            </span>
+                          )}
+                          {presenter.name}
+                        </h4>
+                        <i className="text-lg">
+                          {s.start} - {s.end}{" "}
+                          {presenter.subheading && `| ${presenter.subheading}`}
+                        </i>
+                      </div>
+                    );
+                  })}
+                </div>
+              ))}
+            </div>
+          );
+        })}
       </div>
       <button
         className="elevate absolute left-10 top-10 rounded-lg bg-black px-4 py-2 text-base text-white"
--- app/[lang]/print/page.tsx
+++ app/[lang]/print/page.tsx
@@ -24,62 +24,8 @@ const Print = (props: PrintPropsType) => {
         {params.lang === "cz" ? "Switch to English" : "Přepnout do češtiny"}
       </Link>
       <div className="watermark3 flex h-full flex-col items-center justify-center gap-4">
-        <h2 className="text-3xl">{lang.print.title}</h2>
-        <div className="flex flex-wrap justify-center gap-4">
-          <Link
-            href={`./print/pricelist/tickets`}
-            className="nav rounded-2xl bg-[var(--black)] p-4 px-8 text-lg text-[var(--white)]"
-          >
-            {lang.print.categories.tickets}
-          </Link>
-          <Link
-            href={`./print/pricelist/merch`}
-            className="nav rounded-2xl bg-[var(--black)] p-4 px-8 text-lg text-[var(--white)]"
-          >
-            {lang.print.categories.merch}
-          </Link>
-          <Link
-            href={`./print/pricelist/drinks`}
-            className="nav rounded-2xl bg-[var(--black)] p-4 px-8 text-lg text-[var(--white)]"
-          >
-            {lang.print.categories.drinks}
-          </Link>
-          <Link
-            href={`./print/pricelist/rental`}
-            className="nav rounded-2xl bg-[var(--black)] p-4 px-8 text-lg text-[var(--white)]"
-          >
-            {lang.print.categories.rental}
-          </Link>
-        </div>
         {Object.entries(lang.programDays).map(([k, v]) => (
           <React.Fragment key={k}>
-            <h2 className="text-3xl">{v.name}</h2>
-            <div className="flex flex-wrap justify-center gap-4">
-              <Link
-                href={`./print/${k}/overview`}
-                className="nav rounded-2xl bg-[var(--black)] p-4 px-8 text-lg text-[var(--white)]"
-              >
-                {lang.programCategory.overview}
-              </Link>
-              <Link
-                href={`./print/${k}/robotarna`}
-                className="nav rounded-2xl bg-[var(--black)] p-4 px-8 text-lg text-[var(--white)]"
-              >
-                Robotárna
-              </Link>
-              <Link
-                href={`./print/${k}/stolarna`}
-                className="nav rounded-2xl bg-[var(--black)] p-4 px-8 text-lg text-[var(--white)]"
-              >
-                {lang.programCategory.workshop}
-              </Link>
-              <Link
-                href={`./print/${k}/kaple`}
-                className="nav rounded-2xl bg-[var(--black)] p-4 px-8 text-lg text-[var(--white)]"
-              >
-                {lang.programCategory.lecture}
-              </Link>
-            </div>
             <h2 className="text-3xl">{v.name} [INSTAGRAM]</h2>
             <div className="flex flex-wrap justify-center gap-4">
               <Link
