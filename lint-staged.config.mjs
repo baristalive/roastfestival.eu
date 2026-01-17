2fa3c0c234166a17f017e5b7f38da41d10db1210
Tomas Coufal <tcoufal@redhat.com>
2026-01-17 19:45:23 +0100

    chore: fix lint staged config
    Signed-off-by: Tomas Coufal <tcoufal@redhat.com>

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,7 +1,7 @@
 import path from "path";
 
 const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(" --file ")}`;
+  `npm run lint -- --fix ${filenames.map((f) => path.relative(process.cwd(), f)).join(" ")}`;
 
 const lintStagedConfig = {
   "*": [buildEslintCommand, "prettier --ignore-unknown --write"],
